0a0001daa5ce09881894323b1058502194d23336
DDuarte <dnpd.dd@gmail.com>
2015-12-02 04:34:34 +0000

    Demo github auth with firebase

--- Stockswagen/www/js/app.js
+++ Stockswagen/www/js/app.js
@@ -3,7 +3,37 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
+
+.factory('Auth', function($firebaseAuth) {
+  var endPoint = 'https://stockswagen.firebaseio.com';
+  var usersRef = new Firebase(endPoint);
+  return $firebaseAuth(usersRef);
+})
+
+.controller('AppCtrl', function($scope, Auth) {
+  $scope.login = function(authMethod) {
+    Auth.$authWithOAuthRedirect(authMethod).then(function(authData) {
+    }).catch(function(error) {
+      if (error.code === 'TRANSPORT_UNAVAILABLE') {
+        Auth.$authWithOAuthPopup(authMethod).then(function(authData) {
+        });
+      } else {
+        console.log(error);
+      }
+    });
+  };
+  
+  Auth.$onAuth(function(authData) {
+    if (authData === null) {
+      console.log('Not logged in yet');
+    } else {
+      console.log('Logged in as', authData.uid);
+    }
+    // This will display the user's name in our view
+    $scope.authData = authData;
+  });
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
