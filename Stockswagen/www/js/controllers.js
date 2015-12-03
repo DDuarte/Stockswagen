2b94516f9ef978e9fee0d93544a0303a8dbb99b5
DDuarte <dnpd.dd@gmail.com>
2015-12-03 02:39:13 +0000

    Semi working profile page

--- Stockswagen/www/js/controllers.js
+++ Stockswagen/www/js/controllers.js
@@ -88,6 +88,88 @@ angular.module('starter.controllers', [])
     // This will display the user's name in our view
     $scope.authData = authData;
   });
+
+  // Form data for the login modal
+  $scope.loginData = {};
+  $scope.isExpanded = false;
+  $scope.hasHeaderFabLeft = false;
+  $scope.hasHeaderFabRight = false;
+
+  var navIcons = document.getElementsByClassName('ion-navicon');
+  for (var i = 0; i < navIcons.length; i++) {
+    navIcons.addEventListener('click', function() {
+      this.classList.toggle('active');
+    });
+  }
+
+  ////////////////////////////////////////
+  // Layout Methods
+  ////////////////////////////////////////
+
+  $scope.hideNavBar = function() {
+    document.getElementsByTagName('ion-nav-bar')[0].style.display = 'none';
+  };
+
+  $scope.showNavBar = function() {
+    document.getElementsByTagName('ion-nav-bar')[0].style.display = 'block';
+  };
+
+  $scope.noHeader = function() {
+    var content = document.getElementsByTagName('ion-content');
+    for (var i = 0; i < content.length; i++) {
+      if (content[i].classList.contains('has-header')) {
+        content[i].classList.toggle('has-header');
+      }
+    }
+  };
+
+  $scope.setExpanded = function(bool) {
+    $scope.isExpanded = bool;
+  };
+
+  $scope.setHeaderFab = function(location) {
+    var hasHeaderFabLeft = false;
+    var hasHeaderFabRight = false;
+
+    switch (location) {
+      case 'left':
+        hasHeaderFabLeft = true;
+        break;
+      case 'right':
+        hasHeaderFabRight = true;
+        break;
+    }
+
+    $scope.hasHeaderFabLeft = hasHeaderFabLeft;
+    $scope.hasHeaderFabRight = hasHeaderFabRight;
+  };
+
+  $scope.hasHeader = function() {
+    var content = document.getElementsByTagName('ion-content');
+    for (var i = 0; i < content.length; i++) {
+      if (!content[i].classList.contains('has-header')) {
+        content[i].classList.toggle('has-header');
+      }
+    }
+
+  };
+
+  $scope.hideHeader = function() {
+    $scope.hideNavBar();
+    $scope.noHeader();
+  };
+
+  $scope.showHeader = function() {
+    $scope.showNavBar();
+    $scope.hasHeader();
+  };
+
+  $scope.clearFabs = function() {
+    var fabs = document.getElementsByClassName('button-fab');
+    if (fabs.length && fabs.length > 1) {
+      fabs[0].remove();
+    }
+  };
 })
 
 .controller('PortfolioCtrl', function($scope) {
@@ -102,4 +184,29 @@ angular.module('starter.controllers', [])
 })
 
 .controller('StockCtrl', function($scope, $stateParams) {
+})
+
+.controller('ProfileCtrl', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+  // Set Header
+  $scope.$parent.showHeader();
+  $scope.$parent.clearFabs();
+  $scope.isExpanded = false;
+  $scope.$parent.setExpanded(false);
+  $scope.$parent.setHeaderFab(false);
+
+  // Set Motion
+  $timeout(function() {
+    ionicMaterialMotion.slideUp({
+      selector: '.slide-up'
+    });
+  }, 300);
+
+  $timeout(function() {
+    ionicMaterialMotion.fadeSlideInRight({
+      startVelocity: 3000
+    });
+  }, 700);
+
+  // Set Ink
+  ionicMaterialInk.displayEffect();
 });
