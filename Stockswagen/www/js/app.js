e23e7d3e64721ac09ee2e87854b87c5115027df4
DDuarte <dnpd.dd@gmail.com>
2015-12-15 14:39:49 +0000

    Fix an error where cordova.plugins might not be set

--- Stockswagen/www/js/app.js
+++ Stockswagen/www/js/app.js
@@ -52,7 +52,7 @@ angular.module('starter', ['ionic', 'firebase', 'ionic.service.core', 'ionic.ser
 
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
     if(window.StatusBar) {
